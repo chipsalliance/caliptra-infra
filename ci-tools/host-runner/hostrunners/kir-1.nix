d200796954477b2f5c6b36eba05ae31b203057df
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2026-01-30 14:50:14 -0800

    Swap kir-1 core FPGAs to subsystem (#16)
    This is to make up for the two fpgas moved to dev.

--- ci-tools/host-runner/hostrunners/kir-1.nix
+++ ci-tools/host-runner/hostrunners/kir-1.nix
@@ -23,11 +23,11 @@ in
       ftdi = "1-1.3.1.3";
       sdwire = "1-1.3.1.4";
     })
-    (fpga_service.mkVckCoreJob "caliptra-kir-vck-1" {
+    (fpga_service.mkVckSubsystemJob "caliptra-kir-vck-1" {
       ftdi = "1-1.3.1.1";
       sdwire = "1-1.3.1.2";
     })
-    (fpga_service.mkVckCoreJob "caliptra-kir-vck-4" {
+    (fpga_service.mkVckSubsystemJob "caliptra-kir-vck-4" {
       ftdi = "1-1.3.3";
       sdwire = "1-1.3.4";
     })
