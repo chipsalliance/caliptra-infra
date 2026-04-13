e3fb704a4d80468eed48892f8c714c24c597e91c
Carl Lundin <108372512+clundin25@users.noreply.github.com>
2026-04-13 11:45:54 -0700

    [github-runner]: Add more telemetry (#54)
    Sometimes jobs get stuck and it seems the host created for it
    disappears. Add more logs to see if we can root cause the issue.

--- ci-tools/github-runner/scripts/launch_runner.sh
+++ ci-tools/github-runner/scripts/launch_runner.sh
@@ -2,8 +2,31 @@
 
 #!/bin/bash
 
+function set_guest_attr()
+{
+    curl -X PUT --data "$2" "http://metadata.google.internal/computeMetadata/v1/instance/guest-attributes/caliptra-github-ci/$1" -H "Metadata-Flavor: Google"
+}
+
+set -x
+echo "Starting GitHub Runner launch script"
+set_guest_attr "runner-state" "STARTING"
+
 cd /home/runner
 su runner -l -c "export"
-su runner -l -c "/home/runner/actions-runner/run.sh --jitconfig '${JITCONFIG}'"
 
+echo "Executing run.sh"
+# Run the runner and capture the exit code
+su runner -l -c "/home/runner/actions-runner/run.sh --jitconfig '${JITCONFIG}'"
+exit_code=$?
+
+echo "run.sh exited with code $exit_code"
+if [ $exit_code -eq 0 ]; then
+    set_guest_attr "runner-state" "SUCCESS"
+else
+    set_guest_attr "runner-state" "FAILURE"
+    set_guest_attr "runner-error" "exit-code-$exit_code"
+fi
+
+# Give a small window for the attribute to persist before shutdown
+sleep 5
 shutdown -h now
